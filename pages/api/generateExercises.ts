cbc17495ccf51c6aa28c534757be5d6f49651c9b
Colton Kaiser <kaiserc2@seattleu.edu>
2023-03-30 17:58:08 -0600

    feat: :card_file_box: create generateExercises API route.

---
+++ pages/api/generateExercises.ts
@@ -0,0 +1,39 @@
+import openAiConfig from "@/lib/openAiConfig";
+import { NextApiRequest, NextApiResponse } from "next";
+import { OpenAIApi } from "openai";
+
+const openai = new OpenAIApi(openAiConfig);
+
+const generateExercises = async (req: NextApiRequest, res: NextApiResponse) => {
+  const difficultyLevel = req.body.difficultyLevel || "beginner";
+  const language = req.body.language || "English";
+
+  try {
+    const completion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "system",
+          content: `You are ChatGPT, a large language model trained by OpenAI. You are an expert in all languages. Generate a list of ${language} phrases that you would consider ${difficultyLevel} level.`,
+        },
+      ],
+      temperature: 0.5,
+      max_tokens: 100,
+    });
+
+    const exercises = completion.data.choices[0].message.content;
+    res.status(200).json({ exercises });
+  } catch (error) {
+    if (error.response) {
+      res.status(error.response.status).json(error.response.data);
+    } else {
+      res.status(500).json({
+        error: {
+          message: "An error occurred during your request.",
+        },
+      });
+    }
+  }
+};
+
+export default generateExercises;
