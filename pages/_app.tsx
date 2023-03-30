98a08559fbe88fb09dad755921f4b908027c1ecf
Colton Kaiser <kaiserc2@seattleu.edu>
2023-03-30 17:53:53 -0600

    feat: :sparkles: add SessionProvider and ApolloProvider.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,20 @@
 import type { AppProps } from "next/app";
-import "@/styles/globals.css";
+import { SessionProvider } from "next-auth/react";
+import { Session } from "next-auth";
+import { ApolloProvider } from "@apollo/client";
+import client from "@/lib/apollo-client";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps<{
+  session: Session;
+}>) {
+  return (
+    <SessionProvider session={session}>
+      <ApolloProvider client={client}>
+          <Component {...pageProps} />
+      </ApolloProvider>
+    </SessionProvider>
+  );
 }
