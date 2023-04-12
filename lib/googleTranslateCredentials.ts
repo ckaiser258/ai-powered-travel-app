944db2fa4f8968921201e381491b7bf869273fce
Colton Kaiser <kaiserc2@seattleu.edu>
2023-04-12 15:22:05 -0600

    feat: :sparkles: set up google translate API and route.

---
+++ lib/googleTranslateCredentials.ts
@@ -0,0 +1,14 @@
+const googleTranslateCredentials = {
+  type: process.env.GOOGLE_ACCOUNT_TYPE,
+  project_id: process.env.GOOGLE_PROJECT_ID,
+  private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+  private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  client_email: process.env.GOOGLE_CLIENT_EMAIL,
+  client_id: process.env.GOOGLE_CLIENT_ID,
+  auth_uri: process.env.GOOGLE_AUTH_URI,
+  token_uri: process.env.GOOGLE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL,
+};
+
+export default googleTranslateCredentials;
---
+++ pages/api/googleTranslate.ts
@@ -0,0 +1,103 @@
+import googleTranslateCredentials from "@/lib/googleTranslateCredentials";
+import { DetectResult, Translate } from "@google-cloud/translate/build/src/v2";
+import { NextApiRequest, NextApiResponse } from "next";
+
+const CREDENTIALS = googleTranslateCredentials;
+
+const translate = new Translate({
+  credentials: CREDENTIALS,
+  projectId: CREDENTIALS.project_id,
+});
+
+interface DetectLanguageData {
+  DetectResult?: [DetectResult, any];
+  error?: {
+    message: string;
+  };
+}
+
+interface TranslateTextData {
+  response?: string;
+  error?: {
+    message: string;
+  };
+}
+
+const detectLanguage = async (
+  req: NextApiRequest,
+  res: NextApiResponse<DetectLanguageData>
+) => {
+  if (!CREDENTIALS.private_key) {
+    res.status(500).json({
+      error: {
+        message: "Google Translate API key not configured",
+      },
+    });
+    return;
+  }
+
+  const text = req.body.text || "";
+
+  if (text.trim().length === 0) {
+    res.status(400).json({
+      error: {
+        message: "Please enter a valid input",
+      },
+    });
+    return;
+  }
+
+  try {
+    const response = await translate.detect(text);
+    return response[0].language;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const translateText = async (
+  req: NextApiRequest,
+  res: NextApiResponse<TranslateTextData>
+) => {
+  if (!CREDENTIALS.private_key) {
+    res.status(500).json({
+      error: {
+        message: "Google Translate API key not configured",
+      },
+    });
+    return;
+  }
+
+  const text = req.body.text || "";
+  const targetLanguage = req.body.targetLanguage || "";
+
+  if (text.trim().length === 0) {
+    res.status(400).json({
+      error: {
+        message: "Please enter a valid input",
+      },
+    });
+    return;
+  }
+
+  if (targetLanguage.trim().length === 0) {
+    res.status(400).json({
+      error: {
+        message: "Please enter a valid target language",
+      },
+    });
+    return;
+  }
+
+  try {
+    const [response] = await translate.translate(text, targetLanguage);
+    res.status(200).json({ response });
+  } catch (error) {
+    console.error(error.message);
+    res
+      .status(500)
+      .json({ error: { message: "An error occurred during your request." } });
+  }
+};
+
+export { detectLanguage, translateText };
