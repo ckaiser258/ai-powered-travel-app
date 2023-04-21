2d9340705155253a1682832940c9947f465f7589
Colton Kaiser <kaiserc2@seattleu.edu>
2023-04-21 16:32:38 -0600

    feat: :sparkles: modify checkAnswer query to use Levenshtein distance.

--- db/exercise/queries/checkAnswer.ts
+++ db/exercise/queries/checkAnswer.ts
@@ -1,4 +1,6 @@
-const checkAnswer = async (input: string, language: string) => {
+import levenshtein from "fast-levenshtein";
+
+const checkAnswer = async (input: string, language: string, phrase: string) => {
   try {
     const response = await fetch("api/googleTranslate", {
       method: "POST",
@@ -13,8 +15,29 @@ const checkAnswer = async (input: string, language: string) => {
         data.error || new Error(`Request failed with status ${response.status}`)
       );
     }
-    console.log(data.response);
-    return data.response;
+
+    // Calculate Levenshtein distance
+    const distance = levenshtein.get(data.response, phrase);
+
+    // Determine the similarity threshold (20% of the length of the longer string)
+    const similarityThreshold =
+      Math.max(data.response.length, phrase.length) * 0.2;
+
+    // If the distance is within the threshold, then the user's translation is close enough to the generated phrase.
+    return distance <= similarityThreshold;
+
+    // If it's found that the punctuation differences between the ChatGPT API generated phrases and the Google Translate API generated phrases
+    // are consistently causing issues in the similarity calculation, then the following code can be used instead of the above code
+    // to strip the punctuation from the ChatGPT API generated phrase and the Google Translate API generated phrase
+    // before comparing them.
+
+    // const strippedResponse = data.response.replace(/[^\p{L}\p{N}\s]+/gu, "");
+    // const strippedPhrase = phrase.replace(/[^\p{L}\p{N}\s]+/gu, "");
+
+    // const distance = levenshtein.get(strippedResponse, strippedPhrase);
+    // const similarityThreshold =
+    //   Math.max(strippedResponse.length, strippedPhrase.length) * 0.2;
+    // return distance <= similarityThreshold;
   } catch (error) {
     console.error(error);
     alert(error.message);
