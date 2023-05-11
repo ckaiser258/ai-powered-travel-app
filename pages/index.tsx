65a89f5e8777fe7be3f2d59e118c84f8e3212e60
Colton Kaiser <kaiserc2@seattleu.edu>
2023-05-10 23:29:19 -0600

    feat: :sparkles: add title and center translate form.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,19 @@
 import TranslateForm from "@/components/languageTranslations/TranslateForm";
+import { Box, Typography } from "@mui/material";
 import Head from "next/head";
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Head>
-        <title>AI Travel App</title>
+        <title>AI Travel Assistant</title>
       </Head>
-
-      <TranslateForm />
-    </div>
+      <Typography variant="h2" align="center" mt={2}>
+        AI Powered Travel Assistant
+      </Typography>
+      <Box display="flex" justifyContent="center" alignItems="center">
+        <TranslateForm />
+      </Box>
+    </>
   );
 }
