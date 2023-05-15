674a47181bb2a016e321fc5b6789817ff46ea5fd
Colton Kaiser <kaiserc2@seattleu.edu>
2023-05-15 17:55:45 -0600

    feat: :sparkles: remove title from homepage.

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,9 +8,6 @@ export default function Home() {
       <Head>
         <title>AI Travel Assistant</title>
       </Head>
-      <Typography variant="h2" align="center" my={2}>
-        AI Powered Travel Assistant
-      </Typography>
       <TranslateForm />
     </>
   );
