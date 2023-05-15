5d45c1cc5adc26005eefb866bcbb0b789b9c94ae
Colton Kaiser <kaiserc2@seattleu.edu>
2023-05-15 17:34:17 -0600

    feat: :sparkles: add styling and loading skeletons to common phrases page.

--- pages/common-phrases/index.tsx
+++ pages/common-phrases/index.tsx
@@ -1,5 +1,16 @@
-import { Input } from "@mui/material";
+import {
+  Box,
+  Button,
+  List,
+  ListItem,
+  ListItemText,
+  Skeleton,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
 import { NextPage } from "next";
+import Head from "next/head";
 import { useState } from "react";
 import ReactGoogleAutocomplete from "react-google-autocomplete";
 import { useForm, SubmitHandler } from "react-hook-form";
@@ -12,6 +23,45 @@ interface FormValues {
 // we need to use NEXT_PUBLIC_ prefix to make sure it's exposed.
 // Therefore it's important to limit its domain usage on the Google Cloud Console side of things.
 const GOOGLE_MAPS_KEY = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
+
+const SkeletonList = () => {
+  const skeletonArray: JSX.Element[] = [];
+  for (let i = 0; i < 10; i++) {
+    skeletonArray.push(
+      <Box
+        my={1}
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        key={i}
+      >
+        <Skeleton
+          variant="circular"
+          width={20}
+          height={20}
+          animation="wave"
+          sx={{ mr: 1 }}
+        />
+        <Skeleton width={500} animation="wave" />
+      </Box>
+    );
+  }
+
+  return (
+    <List
+      dense
+      sx={{
+        marginTop: 5,
+      }}
+    >
+      {skeletonArray.map((skeleton, index) => (
+        <ListItem key={index}>
+          <ListItemText primary={skeleton} />
+        </ListItem>
+      ))}
+    </List>
+  );
+};
 
 const CommonPhrasesPage: NextPage = () => {
   const {
@@ -59,47 +109,61 @@ const CommonPhrasesPage: NextPage = () => {
 
     const bulletPoints = result.split("\n");
     return (
-      <ul>
+      <List
+        sx={{
+          marginTop: 5,
+        }}
+      >
         {bulletPoints.map((bulletPoint, index) =>
-          bulletPoint ? <li key={index}>{bulletPoint}</li> : <br key={index} />
+          bulletPoint ? (
+            <ListItem key={index}>
+              <ListItemText primary={bulletPoint} />
+            </ListItem>
+          ) : null
         )}
-      </ul>
+      </List>
     );
   }
 
   return (
-    <div>
+    <>
+      <Head>
+        <title>AI Powered Travel Assistant | Common Phrases Generator</title>
+      </Head>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <h3>Phrases to know:</h3>
-        <Input
-          placeholder="Enter Any Location"
-          fullWidth
-          {...register("location", { required: true })}
-          inputComponent={({ inputRef, onFocus, onBlur, ...props }) => (
-            <ReactGoogleAutocomplete
-              apiKey={GOOGLE_MAPS_KEY}
-              options={{
-                types: ["(regions)"],
-              }}
-              onPlaceSelected={(selected) =>
-                setValue("location", selected.formatted_address)
-              }
-              {...props}
-            />
-          )}
-        />
-
-        {errors.location && (
-          <span style={{ color: "red" }}>Please enter location.</span>
-        )}
-        <br />
-        <br />
-        <input type="submit" value="Generate Common Phrases" />
+        <Stack spacing={4} maxWidth={500} minWidth={300} alignItems="center">
+          <Typography variant="h3">Common Phrases</Typography>
+          <Typography variant="h5">Enter a Location</Typography>
+          <TextField
+            label="Location"
+            fullWidth
+            variant="standard"
+            error={!!errors.location}
+            helperText="Enter any location in the world..."
+            {...register("location", { required: true })}
+            InputProps={{
+              inputComponent: ({ inputRef, onFocus, onBlur, ...props }) => (
+                <ReactGoogleAutocomplete
+                  apiKey={GOOGLE_MAPS_KEY}
+                  options={{
+                    types: ["(regions)"],
+                  }}
+                  onPlaceSelected={(selected) =>
+                    setValue("location", selected.formatted_address)
+                  }
+                  {...props}
+                />
+              ),
+            }}
+          />
+          <Button type="submit" variant="contained">
+            Generate Common Phrases
+          </Button>
+        </Stack>
       </form>
-      <br />
-      {loading && <div>Loading...</div>}
+      {loading && <SkeletonList />}
       {renderBulletPoints()}
-    </div>
+    </>
   );
 };
 
