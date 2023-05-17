8859e4527b8d264083d5316487ba4fa2d1d1cddf
Colton Kaiser <kaiserc2@seattleu.edu>
2023-05-16 20:09:45 -0600

    feat: :sparkles: style and format exercise cards and skeletons.

--- components/exercises/ExerciseList.tsx
+++ components/exercises/ExerciseList.tsx
@@ -1,20 +1,109 @@
 import ExerciseForm from "./ExerciseForm";
+import {
+  Card,
+  CardActions,
+  CardContent,
+  Grid,
+  Typography,
+  Skeleton,
+} from "@mui/material";
 
 interface ExerciseListProps {
   exercises: any[];
   language: string;
+  loading: boolean;
 }
 
-const ExerciseList: React.FC<ExerciseListProps> = ({ exercises, language }) => {
+const SkeletonExerciseList = () => {
+  const skeletonArray: JSX.Element[] = [];
+  for (let i = 0; i < 10; i++) {
+    skeletonArray.push(
+      <Grid
+        item
+        xs={12}
+        md={4}
+        key={i}
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Card
+          sx={{
+            width: 300,
+            height: 200,
+          }}
+        >
+          <CardContent
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              height: "100%",
+              justifyContent: "space-around",
+            }}
+          >
+            <Typography variant="h5">
+              <Skeleton width={250} animation="wave" />
+              <Skeleton width={200} animation="wave" />
+            </Typography>
+
+            <Skeleton
+              width={200}
+              height={40}
+              animation="wave"
+              variant="rectangular"
+            />
+          </CardContent>
+        </Card>
+      </Grid>
+    );
+  }
+
+  return <>{skeletonArray}</>;
+};
+
+const ExerciseList: React.FC<ExerciseListProps> = ({
+  exercises,
+  language,
+  loading,
+}) => {
   return (
-    <ul>
-      {exercises.map((exercise) => (
-        <li key={exercise.id}>
-          <p>{exercise}</p>
-          <ExerciseForm language={language} phrase={exercise} />
-        </li>
-      ))}
-    </ul>
+    <Grid
+      container
+      flexDirection="row"
+      justifyContent="center"
+      spacing={2}
+      mb={2}
+    >
+      {loading ? (
+        <SkeletonExerciseList />
+      ) : (
+        exercises.map((exercise) => (
+          <Grid
+            item
+            xs={12}
+            md={4}
+            key={exercise.id}
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Card
+              sx={{
+                width: 300,
+                height: 200,
+              }}
+            >
+              <CardContent>
+                <Typography variant="h5">{exercise}</Typography>
+              </CardContent>
+              <CardActions>
+                <ExerciseForm language={language} phrase={exercise} />
+              </CardActions>
+            </Card>
+          </Grid>
+        ))
+      )}
+    </Grid>
   );
 };
 
