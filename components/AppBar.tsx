79a2a16b2ac326486d017afa8a4b6a0468bdb1e0
Colton Kaiser <kaiserc2@seattleu.edu>
2023-05-19 19:30:11 -0600

    feat: :sparkles: optimize AppBar for mobile.

--- components/AppBar.tsx
+++ components/AppBar.tsx
@@ -1,31 +1,128 @@
 import AuthButtons from "./users/AuthButtons";
-import { Box, AppBar as MuiAppBar, Toolbar } from "@mui/material";
+import {
+  Box,
+  Divider,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  AppBar as MuiAppBar,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import Link from "./Link";
+import { useState } from "react";
+import { Menu } from "@mui/icons-material";
+
+const drawerWidth = 240;
+const navLinks = [
+  { title: `Home`, path: `/` },
+  { title: `Common Phrases Generator`, path: `/common-phrases` },
+  { title: `Language Exercise Generator`, path: `/exercises` },
+  { title: `Translator`, path: `/` },
+  {
+    title: `Got other questions about travel? Ask our Chat Bot!`,
+    path: `/chat-bot`,
+  },
+];
 
 const AppBar: React.FC = () => {
+  const [mobileOpen, setMobileOpen] = useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} textAlign="center">
+      <Typography variant="h6" sx={{ my: 2 }}>
+        AI Powered Travel Assistant
+      </Typography>
+      <Divider variant="middle" />
+      <List>
+        {navLinks.map(({ title, path }, i) => (
+          <ListItem
+            key={i}
+            disablePadding
+            sx={{
+              display: "block",
+            }}
+          >
+            <Link href={path} underline="none" color="inherit">
+              <ListItemButton sx={{ textAlign: "center" }} disableRipple>
+                <ListItemText primary={title} />
+              </ListItemButton>
+            </Link>
+          </ListItem>
+        ))}
+        <ListItem
+          disablePadding
+          sx={{
+            justifyContent: "center",
+            mt: 1,
+          }}
+        >
+          <AuthButtons />
+        </ListItem>
+      </List>
+    </Box>
+  );
+
   return (
-    <MuiAppBar position="static">
-      <Toolbar sx={{ justifyContent: "space-between" }}>
-        <Box>
-          <Link href="/" color="inherit" underline="none">
-            Home
-          </Link>
-        </Box>
-        <Link href="/common-phrases" color="inherit" underline="none">
-          Common Phrases Generator
-        </Link>
-        <Link href="/exercises" color="inherit" underline="none">
-          Language Exercise Generator
-        </Link>
-        <Link href="/" color="inherit" underline="none">
-          Translator
-        </Link>
-        <Link href="/chat-bot" color="inherit" underline="none">
-          Got other questions about travel? Ask our Chat Bot!
-        </Link>
-        <AuthButtons />
-      </Toolbar>
-    </MuiAppBar>
+    <>
+      <MuiAppBar component="nav" position="sticky">
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { xl: "none" } }}
+          >
+            <Menu />
+          </IconButton>
+          <Typography variant="h6" component="div" flexGrow={1}>
+            AI Powered Travel Assistant
+          </Typography>
+          <Box display={{ xs: "none", xl: "block" }}>
+            {navLinks.map(({ title, path }, i) => (
+              <Link
+                underline="none"
+                href={path}
+                sx={{ color: "primary.contrastText", mx: 2 }}
+                key={i}
+              >
+                {title}
+              </Link>
+            ))}
+          </Box>
+          <Box display={{ xs: "none", sm: "block" }}>
+            <AuthButtons />
+          </Box>
+        </Toolbar>
+      </MuiAppBar>
+      <Box component="nav">
+        <Drawer
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: "block", xl: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+    </>
   );
 };
 
