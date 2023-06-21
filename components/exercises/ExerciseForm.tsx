95c8acaf1c185b0dca6c7e2e6be21c3ddbf95002
Colton <kaiserc2@seattleu.edu>
2023-06-21 12:51:47 -0600

    feat: :sparkles: blast confetti when correct answer given.

--- components/exercises/ExerciseForm.tsx
+++ components/exercises/ExerciseForm.tsx
@@ -1,5 +1,6 @@
 import checkAnswer from "@/db/exercise/queries/checkAnswer";
 import { Button, Stack, TextField } from "@mui/material";
+import confetti from "canvas-confetti";
 import { useEffect, useState } from "react";
 import { useForm, SubmitHandler, Controller } from "react-hook-form";
 
@@ -12,6 +13,30 @@ interface FormValues {
   textToTranslate: string;
 }
 
+const blastConfetti = () => {
+  const duration = 2000;
+  const animationEnd = Date.now() + duration;
+
+  const interval = setInterval(() => {
+    const timeLeft = animationEnd - Date.now();
+
+    if (timeLeft <= 0) {
+      return clearInterval(interval);
+    }
+
+    confetti({
+      angle: 60,
+      spread: 55,
+      origin: { x: 0 },
+    });
+    confetti({
+      angle: 120,
+      spread: 55,
+      origin: { x: 1 },
+    });
+  }, 250);
+};
+
 const ExerciseForm: React.FC<ExerciseFormProps> = ({ language, phrase }) => {
   const {
     handleSubmit,
@@ -23,6 +48,9 @@ const ExerciseForm: React.FC<ExerciseFormProps> = ({ language, phrase }) => {
   const onSubmit: SubmitHandler<FormValues> = async (data) => {
     await checkAnswer(data.textToTranslate, language, phrase).then((data) => {
       setIsCorrectAnswer(data);
+      if (data) {
+        blastConfetti();
+      }
     });
   };
 
