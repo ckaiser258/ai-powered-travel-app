9aa1b17e94cfa996cc00c76494945afc37fe22e1
Colton <kaiserc2@seattleu.edu>
2023-06-24 20:49:26 -0600

    refactor: :recycle: remove unnecessary onClick handler from LoginForm.

--- components/users/LoginForm.tsx
+++ components/users/LoginForm.tsx
@@ -4,18 +4,15 @@ import { useForm } from "react-hook-form";
 
 const LoginForm: React.FC = () => {
   const { handleSubmit } = useForm();
+
   const onSubmit = async () => {
     await signIn("github");
   };
 
   return (
+    // Making this a form in case we want to add more fields later
     <form onSubmit={handleSubmit(onSubmit)}>
-      <Button
-        type="submit"
-        variant="contained"
-        color="secondary"
-        onClick={() => signIn("github")}
-      >
+      <Button type="submit" variant="contained" color="secondary">
         Sign in with GitHub
       </Button>
     </form>
