f037efae48b27a08ae4f834e35cad095271ad9c6
Colton <kaiserc2@seattleu.edu>
2023-07-08 20:47:39 -0600

    fix: :bug: fix OpenAI API not recognizing country codes.

--- pages/api/exercises/generateExercises.ts
+++ pages/api/exercises/generateExercises.ts
@@ -1,12 +1,13 @@
 import openAiConfig from "@/lib/OpenAI/openAiConfig";
 import { NextApiRequest, NextApiResponse } from "next";
 import { OpenAIApi } from "openai";
+import ISO6391 from "iso-639-1";
 
 const openai = new OpenAIApi(openAiConfig);
 
 const generateExercises = async (req: NextApiRequest, res: NextApiResponse) => {
   const difficultyLevel = req.body.difficultyLevel || "beginner";
-  const language = req.body.language || "English";
+  const formattedLanguage = ISO6391.getName(req.body.language) || "English";
 
   try {
     const completion = await openai.createChatCompletion({
@@ -14,7 +15,7 @@ const generateExercises = async (req: NextApiRequest, res: NextApiResponse) => {
       messages: [
         {
           role: "system",
-          content: `You are ChatGPT, a large language model trained by OpenAI. You are an expert in all languages. Generate a list of 10 ${language} phrases that you would consider ${difficultyLevel} level. Respond using markdown. The user is going to guess what they mean. Do not say anything else.`,
+          content: `You are ChatGPT, a large language model trained by OpenAI. You are an expert in all languages. Generate a list of 10 ${formattedLanguage} phrases that you would consider ${difficultyLevel} level. Respond using markdown. The user is going to guess what they mean. Do not say anything else.`,
         },
       ],
       temperature: 0.1,
